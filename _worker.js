f9431d6374eab5016dee41fd479fd34dba8fbca1
xw <164864533+xw68@users.noreply.github.com>
2024-11-16 15:35:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,9 +6,9 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '90cd4a77-141a-43c9-991b-08263cfe9c10';
 
-let proxyIP = '';// 小白勿动，该地址并不影响你的网速，这是给CF代理使用的。'cdn.xn--b6gac.eu.org, cdn-all.xn--b6gac.eu.org'
+let proxyIP = 'cdn-all.xn--b6gac.eu.org';// 小白勿动，该地址并不影响你的网速，这是给CF代理使用的。'cdn.xn--b6gac.eu.org, cdn-all.xn--b6gac.eu.org'
 
-let sub = '';// 避免项目被滥用，现已取消内置订阅器
+let sub = '';// 避免项目被滥用，现已取消内置订阅器 原来是：没有显示原版内容内置优选订阅生成器，可自行搭建 https://github.com/cmliu/WorkerVless2sub就是submake那个
 let subconverter = 'SUBAPI.fxxk.dedyn.io';// clash订阅转换后端，目前使用CM的订阅转换功能。自带虚假uuid和host订阅。
 let subconfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini"; //订阅配置文件
 let subProtocol = 'https';
